b43ec50d069542595049bab5f737458c9024a98a
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2024-12-09 14:23:05 -0600

    Actualizacion diciembre

--- Pages/CopyRight/CopyRight.jsx
+++ Pages/CopyRight/CopyRight.jsx
@@ -1,9 +1,33 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { s } from "./CopyRight.style";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { NavHeaderCopyRight, Txt } from "../../components";
 
 const CopyRight = ({onPressInfo}) => {
+
+    const renderCredits = () => (
+        <>
+            <Txt>Tesis por apoyo a la docencia</Txt>
+            <Txt/>
+            <Txt>Diseño y Programación</Txt>
+            <Txt>Cristian Rodolfo Gómez Mares</Txt>
+            <Txt/>
+            <Txt>Directora del trabajo</Txt>
+            <Txt>Rosalba Rodríguez Chávez</Txt>
+            <Txt/>
+            <Txt>Ejercicios y resolución</Txt>
+            <Txt>Rosalba Rodríguez Chávez</Txt>
+            <Txt/>
+            <Txt>Revisión</Txt>
+            <Txt>Alicia Pineda Ramírez</Txt>
+            <Txt>Aldo Jiménez Arteaga</Txt>
+            <Txt/>
+            <Txt>Coordinación</Txt>
+            <Txt>María del Rocío Ávila Núñez</Txt>
+            <Txt/>
+            <Txt>2024</Txt>
+        </>
+    );
 
     return(
         <>
@@ -15,25 +39,7 @@ const CopyRight = ({onPressInfo}) => {
                         />
                     </View>
                     <View style={s.body}>
-                        <Txt>Tesis por apoyo a la docencia</Txt>
-                        <Txt/>
-                        <Txt>Diseño y Programación</Txt>
-                        <Txt>Cristian Rodolfo Gómez Mares</Txt>
-                        <Txt/>
-                        <Txt>Directora del trabajo</Txt>
-                        <Txt>Rosalba Rodríguez Chávez</Txt>
-                        <Txt/>
-                        <Txt>Ejercicios y resolución</Txt>
-                        <Txt>Rosalba Rodríguez Chávez</Txt>
-                        <Txt/>
-                        <Txt>Revisión</Txt>
-                        <Txt>Alicia Pineda Ramírez</Txt>
-                        <Txt>Aldo Jiménez Arteaga</Txt>
-                        <Txt/>
-                        <Txt>Coordinación</Txt>
-                        <Txt>María del Rocío Ávila Núñez</Txt>
-                        <Txt/>
-                        <Txt>2024</Txt>
+                        <ScrollView>{renderCredits()}</ScrollView>
                     </View>
                 </SafeAreaView>
             </SafeAreaProvider>
